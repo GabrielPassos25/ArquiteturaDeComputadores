6e179e8285bff6f26af747a488e468f101f10705
GabrielPassos25 <gabrielurano30@gmail.com>
2020-05-29 22:43:12 -0300

    Assembler and Emulador

--- exercícios/ExerciciosAula15/montador.py
+++ exercícios/ExerciciosAula15/montador.py
@@ -4,6 +4,7 @@ dicionario_instrucoes = {'nop':[0x01,[]],'iadd':[0x02, []],'isub':[0x05, []],'ia
                      'bipush':[0x19,['byte']],'iload':[0x1c, ['varnum']],'istore':[0x22, ['new_varnum']],'ldc_w':[0x32, ['index']],'goto':[0x3c, ['offset']], #Variáveis que utilizam uma constante
                      'iflt':[0x43, ['offset']],'ifeq':[0x47, ['offset']],'if_icmpeq':[0x4b, ['offset']],'invokevirtual':[0x55, ['disp']],
                      'iinc':[0x36,['varnum', 'const']]} #Instruções que utilizam duas constantes
+
 #Dicionários para marcar as principais coisas
 marcadores = {}
 variaveis = {}
@@ -29,29 +30,22 @@ def main():
             del conteudo[0]
         #Checar se a palavra é uma instrução válida
         if(conteudo[0] in dicionario_instrucoes.keys()):
+            #Assume que a instrução está correta
+            byte_execucao +=1
+            byte.append(dicionario_instrucoes[conteudo[0]][0])
+            #constantes para checagem operadores
             constantes = dicionario_instrucoes[conteudo[0]][1]  #Tipo de constantes
             operando = conteudo[1: len(constantes)+1] #Valores das constantes
-            print(conteudo)
-            print(constantes, operando)
             #Checar se é um operador válido
-            if(operador(constantes, len(constantes), operando)):
-                #Adicionar instrução
-                byte_execucao +=1
-                byte.append(dicionario_instrucoes[conteudo[0]][0])
-            else:
-                print("Operador inválido")
+            if(not operador(constantes, len(constantes), operando)):
+                print("Instrução inválida")
                 return 0
-        else:
-            print("Instrução inválida")
-            return 0
     txt.close()
-    print(marcadores, variaveis, byte)
-    #escrever() #Se não tiver erros, escreve o arquivo
+    escrever() #Se não tiver erros, escreve o arquivo
         
 def marcador(marcador):
     global dicionario_instrucoes, variaveis, byte_execucao 
     if(marcador in dicionario_instrucoes.keys() or marcador in variaveis.keys()): #checar se é uma variável  
-        print("Marcador inválido")
         return 0
     else:
         #Adiciona o marcador 
@@ -61,27 +55,25 @@ def marcador(marcador):
 
 def operador(constantes, num_constantes, operando):
     global variaveis, byte, byte_execucao, quant_variaveis
-    if(num_constantes == len(operando)):
-        #Se num_constantes = 0, obviamente o comando estará certo
-        if(num_constantes == 0):
-            condicao = True
+    if(num_constantes == 0):
+        condicao = True
+    else:
         condicao = True
         for i in range(0,num_constantes): 
             if(constantes[i] == 'varnum'):
                 if(operando[i] not in variaveis):
                     condicao = False
-                elif(constantes[i] == 'new_varnum'):
-                    if(not operando[0] in marcadores.keys()):
-                        condicao = False
-                elif(constantes[i] == 'byte') or (constantes[i] == 'const') or (constantes[i] == 'disp') or (constantes[i] == 'index'):
-                    if(not operando[i].isnumeric()):
-                        condicao = False
-                else: #offset
-                    if(not marcador(operando[i])):
-                        condicao = False
-    else:
-        condicao = False
-        return 0
+            elif(constantes[i] == 'new_varnum'):
+                if(operando[0] in marcadores.keys()):
+                    condicao = False
+            elif(constantes[i] == 'byte') or (constantes[i] == 'const') or (constantes[i] == 'disp') or (constantes[i] == 'index'):
+                if(not operando[i].isnumeric()):
+                    condicao = False
+            else:
+                if(not operando[i] in marcadores):
+                    condicao = False
+    if(condicao == False):
+        return condicao
     #Adiciona o operador
     for i in range(0,num_constantes):
         if(constantes[i] == 'varnum'):
@@ -97,9 +89,11 @@ def operador(constantes, num_constantes, operando):
             byte.append(int(operando[i]))
             byte_execucao +=1
         elif(constantes[i] == 'disp') or (constantes[i] == 'index'):
-            #???????????
+            byte_list.append(int(operand[i]) & 0xff) #Unsigned int 8 bits
+            byte_list.append(int(operand[i]) >> 8)
+            byte_counter += 2
         else: #offset
-            byte.append([operando[i], byte_execucao])
+            byte.append([operando[i], byte_execucao-1])
             byte_execucao +=2
     return 1
 
@@ -111,4 +105,37 @@ def variavel(variavel):
     else:
         return 1
 
+def inicializar(byte, quant_variaveis, output):
+    registradores = [0x7300,0x0006,0x1001,0x0400,0x1001 + quant_variaveis]
+    for r in registradores:
+        output += r.to_bytes(4,byteorder = 'little')
+
+def cabecalho(byte_execucao, output):
+    q = (20 + byte_execucao).to_bytes(4, byteorder = 'little')
+    output += q
+
+def programa(byte,output,marcadores):
+    for i in byte:
+        if type(i) is list:
+            byte_marcador = (marcadores[i[0]] - i[1]) & 0xffff #Unsigned int 16 bits offset = 2bytes 
+            output += byte_marcador.to_bytes(2,byteorder = 'big')
+        else:
+            output.append(i)
+
+def escrita(output):
+    program = open(sys.argv[1] + '.exe', 'wb')
+    program.write(output)
+    program.close()
+
+def escrever():
+    global byte_execucao, byte, linhas, marcadores
+    output = bytearray()
+    #cabeçalho
+    cabecalho(byte_execucao, output)
+    #inicialização
+    inicializar(byte, quant_variaveis, output)
+    #programa
+    programa(byte, output, marcadores)
+    #escrita
+    escrita(output)
 main()
